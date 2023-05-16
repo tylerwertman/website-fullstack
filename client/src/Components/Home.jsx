5b739332126aac3e513fc85d7b05047a87906ede
Tyler <tylerwertman@outlook.com>
2023-05-16 00:03:38 -0400

    mini nav last darkmode

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react'
 import me from './images/me.png'
-import linkedin from './images/Linkedin-fill-DSG.svg'
-import github from './images/Github-fill-DSG.svg'
-import file from './images/File-fill-DSG.svg'
-import linkedinH from './images/Linkedin-fill-SG.svg'
-import githubH from './images/Github-fill-SG.svg'
-import fileH from './images/File-fill-SG.svg'
+import linkedinDSG from './images/Linkedin-fill-DSG.svg'
+import githubDSG from './images/Github-fill-DSG.svg'
+import fileDSG from './images/File-fill-DSG.svg'
+import linkedinLG from './images/Linkedin-fill-LG.svg'
+import githubLG from './images/Github-fill-LG.svg'
+import fileLG from './images/File-fill-LG.svg'
+import linkedinSG from './images/Linkedin-fill-SG.svg'
+import githubSG from './images/Github-fill-SG.svg'
+import fileSG from './images/File-fill-SG.svg'
 
 const Home = (props) => {
     const {darkMode} = props
@@ -28,10 +31,10 @@ const Home = (props) => {
             </div>
             <div id={darkMode?"introTwoDark":"introTwo"}>
                 <br /><h2>Hello, I am Tyler and I am a full-stack web developer</h2><br />
-                <div id="icons">
-                    <a href="https://www.linkedin.com/in/tyler-wertman/" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 0 ? "shadow" : ""} onMouseEnter={()=>{hover(0)}} onMouseOut={hover} src={hoveredElement === 0 ? linkedinH : linkedin} alt="linkedin" /></a>
-                    <a href="https://github.com/tylerwertman" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 1 ? "shadow" : ""} onMouseEnter={()=>{hover(1)}} onMouseOut={hover} src={hoveredElement === 1 ? githubH : github} alt="github" /></a>
-                    <a href="./resume.pdf" target="_blank noopener noreferrer" download="Tyler Wertman Resume"><img className={hoveredElement === 2 ? "shadow" : ""} onMouseEnter={()=>{hover(2)}} onMouseOut={hover} src={hoveredElement === 2 ? fileH : file} alt="file" /></a>
+                <div id={darkMode?"iconsDark":"icons"}>
+                    <a href="https://www.linkedin.com/in/tyler-wertman/" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 0 ? "shadow" : ""} onMouseEnter={()=>{hover(0)}} onMouseOut={hover} src={hoveredElement === 0 ? linkedinSG : darkMode ? linkedinLG : linkedinDSG} alt="linkedin" /></a>
+                    <a href="https://github.com/tylerwertman" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 1 ? "shadow" : ""} onMouseEnter={()=>{hover(1)}} onMouseOut={hover} src={hoveredElement === 1 ? githubSG : darkMode ? githubLG : githubDSG} alt="github" /></a>
+                    <a href="./resume.pdf" target="_blank noopener noreferrer" download="Tyler Wertman Resume"><img className={hoveredElement === 2 ? "shadow" : ""} onMouseEnter={()=>{hover(2)}} onMouseOut={hover} src={hoveredElement === 2 ? fileSG : darkMode ? fileLG : fileDSG} alt="file" /></a>
                     {/* <a href="#" target="_blank noopener noreferrer"><img src="./images/Medium-fill-DSG.svg" alt="Medium"/></a> */}
                     {/* <a href="https://www.etsy.com/shop/TylersTechSpace" target="_blank noopener noreferrer"><img src="./images/Etsy-fill-DSG.svg" alt="Etsy"/></a> */}
                     {/* <a href="https://www.youtube.com/@synergystyles" target="_blank noopener noreferrer"><img src="./images/Youtube-fill-DSG.svg" alt="Youtube"/></a> */}
