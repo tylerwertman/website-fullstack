b5b3ceed871d5dc2de61a0e4cd6d97bc60fff1ff
Tyler <tylerwertman@outlook.com>
2023-05-31 09:19:02 -0400

    project desc

--- client/src/Components/Projects.jsx
+++ client/src/Components/Projects.jsx
@@ -10,13 +10,13 @@ const Projects = (props) => {
             <div className={darkMode ? "contentDark mb-3" : "content mb-3"}>
                 <a href="http://bookclub.tylerw.xyz"><h3>Book Club</h3></a>
                 <p>Built with: Mongoose, Express, React, Node, Bootstrap</p>
-                <p>Features: CRUD, reg/login, protected routes, dark mode, table sorting, many to many relationships</p>
+                <p>Features: CRUD, reg/login, protected routes, dark mode, table sorting by column, many to many relationships</p>
             </div>
 
             <div className={darkMode ? "contentDark mb-3" : "content mb-3"}>
                 <a href="http://brightideas.tylerw.xyz"><h3>Bright Ideas</h3></a>
                 <p>Built with: Mongoose, Express, React, Node, Bootstrap</p>
-                <p>Features: CRUD, reg/login, protected routes, dark mode, many to many relationships</p>
+                <p>Features: CRUD, reg/login, protected routes, dark mode, auto-sort, many to many relationships</p>
             </div>
             <div className={darkMode ? "contentDark" : "content"}>
                 <WTP />
