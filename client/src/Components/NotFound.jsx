fb6933d7f05eae6b98ad837b352ea37263881104
Tyler <tylerwertman@outlook.com>
2023-06-10 23:50:39 -0400

    404

--- client/src/Components/NotFound.jsx
+++ client/src/Components/NotFound.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import missing from './images/404.png'
+import missingDark from './images/404Dark.png'
 
-const NotFound = () => {
+const NotFound = (props) => {
+    const {darkMode} = props
     return (
         <div style={{ marginTop: "60px" }}>
-            <img alt="404 Not Found" src={missing} />
+            <img alt="404 Not Found" src={darkMode ? missingDark : missing} />
         </div>
     )
 }
