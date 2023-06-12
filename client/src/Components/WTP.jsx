8affc10473eedbb983fc71af61ff2df714b7d9b1
Tyler <tylerwertman@outlook.com>
2023-06-12 15:00:37 -0400

    add male/female to start/select & gen2,3

--- client/src/Components/WTP.jsx
+++ client/src/Components/WTP.jsx
@@ -3,7 +3,7 @@ import wtp from './images/WTP.mov'
 import axios from 'axios'
 
 const WTP = () => {
-    let rng = Math.floor((Math.random() * 152) + 1)
+    let rng = Math.floor((Math.random() * 439) + 1)
 
     //make object with name, id, sprites,
     const [pokeObj, setPokeObj] = useState({
@@ -12,12 +12,17 @@ const WTP = () => {
         spriteFD: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${rng}.png`,
         spriteFS: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${rng}.png`,
         spriteBD: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${rng}.png`,
-        spriteBS: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${rng}.png`
+        spriteBS: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/${rng}.png`,
+        spriteFDf: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/female/${rng}.png`,
+        spriteFSf: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/female/${rng}.png`,
+        spriteBDf: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/female/${rng}.png`,
+        spriteBSf: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/female${rng}.png`
     })
 
     const [isHidden, setIsHidden] = useState(true)
     const [isShiny, setIsShiny] = useState(false)
     const [isFront, setIsFront] = useState(true)
+    const [isFemale, setIsFemale] = useState(false)
 
     useEffect(() => {
         axios.get(`https://pokeapi.co/api/v2/pokemon/${rng}`)
@@ -28,32 +33,42 @@ const WTP = () => {
                     spriteFD: response.data.sprites.front_default,
                     spriteFS: response.data.sprites.front_shiny,
                     spriteBD: response.data.sprites.back_default,
-                    spriteBS: response.data.sprites.back_shiny
+                    spriteBS: response.data.sprites.back_shiny,
+                    spriteFDf: response.data.sprites.front_female,
+                    spriteFSf: response.data.sprites.front_shiny_female,
+                    spriteBDf: response.data.sprites.back_female,
+                    spriteBSf: response.data.sprites.back_shiny_female
                 })
-                console.log(response.data)
+                // console.log(response.data)
             })
             .catch(error => {
                 console.log(error)
             })
-            // eslint-disable-next-line
+        // eslint-disable-next-line
     }, [])
 
     const newPoke = () => {
         // console.log("running newPoke");
-        rng = Math.floor((Math.random() * 152) + 1)
+        rng = Math.floor((Math.random() * 439) + 1)
         axios.get(`https://pokeapi.co/api/v2/pokemon/${rng}`)
             .then(response => {
+                console.log(pokeObj)
                 setPokeObj({
                     pName: response.data.name,
                     id: response.data.id,
                     spriteFD: response.data.sprites.front_default,
                     spriteFS: response.data.sprites.front_shiny,
                     spriteBD: response.data.sprites.back_default,
-                    spriteBS: response.data.sprites.back_shiny
+                    spriteBS: response.data.sprites.back_shiny,
+                    spriteFDf: response.data.sprites.front_female,
+                    spriteFSf: response.data.sprites.front_shiny_female,
+                    spriteBDf: response.data.sprites.back_female,
+                    spriteBSf: response.data.sprites.back_shiny_female
                 })
                 setIsHidden(true)
                 setIsFront(true)
                 setIsShiny(false)
+                setIsFemale(false)
                 // setImgSrc(spriteFD)
             })
             .catch(error => {
@@ -80,6 +95,14 @@ const WTP = () => {
         if (!isHidden && isFront) setIsFront(!isFront)
     }
 
+    const femalePoke = () => {
+        if (!isHidden && isFemale && pokeObj.spriteBDf) setIsFemale(!isFemale)
+    }
+    const malePoke = () => {
+        if (!isHidden && !isFemale && pokeObj.spriteBDf) setIsFemale(!isFemale)
+
+    }
+
     const capitalize = (str) => {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
@@ -92,12 +115,16 @@ const WTP = () => {
                 <div className="gameboyScr">
                     <div className="js" >
                         <img src={
-                            !isShiny && isFront ? pokeObj.spriteFD :
-                                isShiny && isFront ? pokeObj.spriteFS :
-                                    !isShiny && !isFront ? pokeObj.spriteBD :
-                                        isShiny && !isFront ? pokeObj.spriteBS : null
+                            !isShiny && isFront && isFemale ? pokeObj.spriteFDf :
+                                isShiny && isFront && isFemale ? pokeObj.spriteFSf :
+                                    !isShiny && !isFront && isFemale ? pokeObj.spriteBDf :
+                                        isShiny && !isFront && isFemale ? pokeObj.spriteBS :
+                                            !isShiny && isFront ? pokeObj.spriteFD :
+                                                isShiny && isFront ? pokeObj.spriteFS :
+                                                    !isShiny && !isFront ? pokeObj.spriteBD :
+                                                        isShiny && !isFront ? pokeObj.spriteBS : null
                         } className={isHidden ? "sillhouette" : null} alt="sillhouette" />
-                        <p id="textBox">{isHidden ? "Who's that Pokemon?" : isShiny ? `It's ${capitalize(pokeObj.pName)} (shiny)` : `It's ${capitalize(pokeObj.pName)}`}</p>
+                        <p id="textBox">{isHidden ? "Who's that Pokemon?" : isShiny && isFemale ? `It's ${capitalize(pokeObj.pName)}     (shiny, female)` : isShiny ? `It's ${capitalize(pokeObj.pName)}     (shiny)` : isFemale ? `It's ${capitalize(pokeObj.pName)}     (female)` :`It's ${capitalize(pokeObj.pName)}`}</p>
                     </div>
                     <br />
                     <p>GAMEBOY <span className="C">C</span><span className="O">O</span><span className="L">L</span><span className="O2">O</span><span className="R">R</span></p>
@@ -122,11 +149,11 @@ const WTP = () => {
                 </div>
                 <div className="ss">
                     <div className="start">
-                        <div className="ssBtn"></div>
+                        <div className="ssBtn activeBtn" onClick={femalePoke}></div>
                         <div className="ssText">Start</div>
                     </div>
                     <div className="select">
-                        <div className="ssBtn"></div>
+                        <div className="ssBtn activeBtn" onClick={malePoke}></div>
                         <div className="ssText">Select</div>
                     </div>
                 </div>
