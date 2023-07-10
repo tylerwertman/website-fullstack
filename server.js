593c9ae6405a51379840f16c472676757c1de5a3
Tyler <114204749+tylerwertman@users.noreply.github.com>
2023-07-09 22:15:02 -0400

    testing

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 8000;
+const express = require('express')
+const app = express()
+const port = 8000
 const cors = require('cors')
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 
-require('dotenv').config();
-require('./server/config/mongoose.config');
+require('dotenv').config()
+require('./server/config/mongoose.config')
 
 app.use(cookieParser())
-app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(express.json(), express.urlencoded({ extended: true }));
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+app.use(express.json(), express.urlencoded({ extended: true }))
 
-require('./server/routes/message.routes')(app);
-require('./server/routes/user.routes')(app);
+require('./server/routes/message.routes')(app)
+require('./server/routes/user.routes')(app)
 
 
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+app.listen(port, () => console.log(`Listening on port: ${port}`))
 
