8c883aaa3d8b8a2a53aeedbb22f941d30ab76c7c
Tyler <114204749+tylerwertman@users.noreply.github.com>
2023-07-11 01:25:41 -0400

    Update node.js.yaml

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const port = 8000
 
 require('dotenv').config()
 require('./server/config/mongoose.config')
@@ -14,4 +15,4 @@ require('./server/routes/message.routes')(app)
 require('./server/routes/user.routes')(app)
 
 
-app.listen(8000, () => console.log(`Listening on port: 8000`))
+app.listen(port, () => console.log(`Listening on port: ${port}`))
