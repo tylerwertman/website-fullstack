e4d0b91dff7d0350ff087821ba3c9562cc7e9888
Tyler <114204749+tylerwertman@users.noreply.github.com>
2023-07-11 00:39:45 -0400

    move server/pack/lock test

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 require('dotenv').config()
-require('./config/mongoose.config')
+require('./server/config/mongoose.config')
 
 app.use(cookieParser())
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json(), express.urlencoded({ extended: true }))
 
-require('./routes/message.routes')(app)
-require('./routes/user.routes')(app)
+require('./server/routes/message.routes')(app)
+require('./server/routes/user.routes')(app)
 
 
 app.listen(8000, () => console.log(`Listening on port: 8000`))
