11b501d8e9919fd118fd9afb4102562702ff0afe
Tyler <114204749+tylerwertman@users.noreply.github.com>
2023-07-12 17:30:28 -0400

    medium

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react'
 import me from './images/me.png'
 import linkedinDSG from './images/Linkedin-fill-DSG.svg'
 import githubDSG from './images/Github-fill-DSG.svg'
+import mediumDSG from './images/Medium-fill-DSG.svg'
 import fileDSG from './images/File-fill-DSG.svg'
 import linkedinLG from './images/Linkedin-fill-LG.svg'
 import githubLG from './images/Github-fill-LG.svg'
+import mediumLG from './images/Medium-fill-LG.svg'
 import fileLG from './images/File-fill-LG.svg'
 import linkedinSG from './images/Linkedin-fill-SG.svg'
 import githubSG from './images/Github-fill-SG.svg'
+import mediumSG from './images/Medium-fill-SG.svg'
 import fileSG from './images/File-fill-SG.svg'
 
 const Home = (props) => {
@@ -34,8 +37,8 @@ const Home = (props) => {
                 <div id={darkMode ? "iconsDark" : "icons"}>
                     <a href="https://www.linkedin.com/in/tyler-wertman/" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 0 ? "shadow" : null} onMouseEnter={() => { hover(0) }} onMouseOut={hover} src={hoveredElement === 0 ? linkedinSG : darkMode ? linkedinLG : linkedinDSG} alt="linkedin" /></a>
                     <a href="https://github.com/tylerwertman" target="_blank noopener noreferrer" rel="noreferrer"><img className={hoveredElement === 1 ? "shadow" : null} onMouseEnter={() => { hover(1) }} onMouseOut={hover} src={hoveredElement === 1 ? githubSG : darkMode ? githubLG : githubDSG} alt="github" /></a>
+                    <a href="https://medium.com/@tylerwertman" target="_blank noopener noreferrer"><img className={hoveredElement === 3 ? "shadow" : null} onMouseEnter={() => { hover(3) }} onMouseOut={hover} src={hoveredElement === 3 ? mediumSG : darkMode ? mediumLG : mediumDSG} alt="Medium"/></a>
                     <a href="/resume.pdf" download="Tyler Wertman Resume"><img className={hoveredElement === 2 ? "shadow" : null} onMouseEnter={() => { hover(2) }} onMouseOut={hover} src={hoveredElement === 2 ? fileSG : darkMode ? fileLG : fileDSG} alt="file" /></a>
-                    {/* <a href="#" target="_blank noopener noreferrer"><img src="./images/Medium-fill-DSG.svg" alt="Medium"/></a> */}
                     {/* <a href="https://www.etsy.com/shop/TylersTechSpace" target="_blank noopener noreferrer"><img src="./images/Etsy-fill-DSG.svg" alt="Etsy"/></a> */}
                     {/* <a href="https://www.youtube.com/@synergystyles" target="_blank noopener noreferrer"><img src="./images/Youtube-fill-DSG.svg" alt="Youtube"/></a> */}
                     {/* <a href="#" target="_blank noopener noreferrer"><img src="./images/Spotify-fill-DSG.svg" alt="Spotify"/></a> */}
