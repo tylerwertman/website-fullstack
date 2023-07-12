0c9590dcef1bd5179f68a15a3a6636b3892e5218
Tyler <114204749+tylerwertman@users.noreply.github.com>
2023-07-11 21:30:45 -0400

    update About, first real CICD test

--- client/src/Components/About.jsx
+++ client/src/Components/About.jsx
@@ -6,7 +6,7 @@ const About = (props) => {
         <div className={darkMode ? "section aboutDark" : "section about"} id="about">
             <h2>About</h2>
             <div className={darkMode ? "contentDark" : "content"}>
-                <p>I was born and raised on Long Island, New York and currently reside in South Florida. My admiration for the cornerstone between form and function runs deep. In tandem with my fascination for learning how things work, my passion for web development began. When I'm not coding, you can find me longboarding at the park, hiking mountains, watching the New York Mets or reading.</p>
+                <p>I was born and raised on Long Island, New York and currently reside in South Florida. My admiration for the cornerstone between form and function runs deep. In tandem with my fascination for learning how things work, my passion for web development began. When I'm not coding, you can find me at the beach, watching the New York Mets, tinkering with my 3D printer or reading.</p>
             </div>
         </div>
     )
